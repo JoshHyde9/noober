da603e9cc72b87d726ac1308aeccbcb05797ab93
Josh Hyde <joshhyde546@gmail.com>
2022-06-26 20:26:08 +1000

    Started map component

---
+++ src/components/HomeMap/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const HomeMap = (props) => {
+  return (
+    <View
+      style={{
+        height: 300,
+        backgroundColor: "#ddd",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>TODO: Create a map</Text>
+    </View>
+  );
+};
