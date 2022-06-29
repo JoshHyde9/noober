925eafeb14fc0b275431dde224f199f23664e7c3
Josh Hyde <joshhyde546@gmail.com>
2022-06-29 21:09:00 +1000

    Added Seach component

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 
 import { HomeMap } from "../../components/HomeMap";
-import { CovidMessage } from "../../components/HomeMap/CovidMessage";
+import { CovidMessage } from "../../components/CovidMessage";
+import { HomeSearch } from "../../components/HomeSearch";
 
 export const HomeScreen = (props) => {
   return (
     <View>
       <HomeMap />
       <CovidMessage />
+      <HomeSearch />
     </View>
   );
 };
